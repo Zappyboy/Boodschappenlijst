515b9839d44125254d2538b32512af411c547709
Zappyboy <helloworld.zapp@gmail.com>
2020-01-07 18:09:44 +0100

    new

--- H4/opdr4.php
+++ H4/opdr4.php
@@ -1,45 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Opdracht 4</title>
+    <meta charset="UTF-8">
+    <title>Add products</title>
 </head>
 <body>
-<button onclick="removeLI()">Remove</button>
-<ul id="list">
-    <li>Aardappelen</li>
-    <li>Aardbeien</li>
-    <li>3 pakken melk</li>
-    <li>yoghurt</li>
-</ul>
-
-<script>
-
-    var items = document.querySelectorAll("#list li"),
-        tab = [], liIndex;
-
-    // populate tab with li data
-    for(var i = 0; i < items.length; i++){
-        tab.push(items[i].innerHTML);
-    }
-
-    // get li index using tab array on li click event
-    for(var i = 0; i < items.length; i++){
-
-        items[i].onclick = function(){
-
-            liIndex = tab.indexOf(this.innerHTML);
-            console.log(this.innerHTML + " INDEX = " + liIndex);
-        };
-
-    }
-
-    function removeLI(){
-
-        items[liIndex].parentNode.removeChild(items[liIndex]);
-
-    }
-
-</script>
-
+<form action="" method="post">
+    <p>
+        <label for="omschrijving">Omschrijving</label>
+        <input type="text" name="omschrijving" id="omschrijving">
+    </p>
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
